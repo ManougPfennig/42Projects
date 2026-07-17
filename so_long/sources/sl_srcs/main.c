63acb032663d66351f383745de433f59b20e1ee8
manoug <manoug.pfen@gmail.com>
2026-07-17 23:04:55 +0200

    all 42 common-core projects

---
+++ so_long/sources/sl_srcs/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapfenni <mapfenni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 10:16:14 by mapfenni          #+#    #+#             */
+/*   Updated: 2023/06/07 18:54:32 by mapfenni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	main(int ac, char **av)
+{
+	t_data	data;
+
+	if (arg_parsing(ac, av, &data))
+		exit(EXIT_FAILURE);
+	start_mlx(&data);
+	create_window(&data, "so_long");
+	create_image(&data);
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, \
+	&data.line_length, &data.endian);
+	data.moves = 0;
+	set_imgs(&data);
+	put_map(&data);
+	mlx_hook(data.win, 2, 1L << 0, key_pressed, &data);
+	mlx_hook(data.win, 3, 1L << 1, key_release, &data);
+	mlx_loop_hook(data.mlx, deal_key, &data);
+	mlx_hook(data.win, 17, 0, ft_destroy_windows, &data);
+	mlx_key_hook(data.win, deal_key, &data);
+	mlx_loop(data.mlx);
+	ft_free_tab(data.map, data.copy_map);
+	exit(EXIT_SUCCESS);
+}
